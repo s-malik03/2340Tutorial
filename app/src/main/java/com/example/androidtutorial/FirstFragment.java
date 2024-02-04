701a029ce78f6c71d4ba39bb32f678f2e92b7803
safiy <safiy.malik@gmail.com>
2024-02-04 01:10:22 -0500

    aaa

--- app/src/main/java/com/example/androidtutorial/FirstFragment.java
+++ app/src/main/java/com/example/androidtutorial/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,14 +15,18 @@ import com.example.androidtutorial.databinding.FragmentFirstBinding;
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    TextView showCountTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        // inflate fragment layout
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        // get count text view
+        showCountTextView = binding.getRoot().findViewById(R.id.count);
+        // return fragment layout
         return binding.getRoot();
 
     }
@@ -29,13 +34,24 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.increment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                increment(view);
             }
         });
+    }
+
+    private void increment(View view) {
+
+        // get value of count number textview
+        String countString = showCountTextView.getText().toString();
+        // convert the string number into integer data type
+        Integer count = Integer.parseInt(countString);
+        // increment the number
+        count++;
+        // convert to string and set on textview
+        showCountTextView.setText(count.toString());
     }
 
     @Override
